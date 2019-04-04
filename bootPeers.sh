c213e95c2e48aa5a5217b6bed379e8d506cca26f
Alexandre <alex.al.mar@hotmail.com>
2019-04-04 11:38:09 +0100

    Added Scripts

---
+++ bootPeers.sh
@@ -0,0 +1,4 @@
+gnome-terminal -e -noclose "sh run.sh 1.0 1"
+gnome-terminal -e -noclose "sh run.sh 1.0 2"
+gnome-terminal -e -noclose "sh run.sh 1.0 3"
+gnome-terminal -e -noclose "sh run.sh 1.0 4"
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+clear
 java -Djava.net.preferIPv4Stack=true -classpath bin peer.Peer "$1" "$2" //localhost/ 224.0.0.0 8000 224.0.0.0 8001 224.0.0.0 8002
