b3280f7fd19fa88aa70c5e6e593aecff54693a78
filipeFLemos <ee121402fe.up.pt>
2019-04-04 14:27:59 +0100

    fix

--- src/channels/MessageHandler.java
+++ src/channels/MessageHandler.java
@@ -43,7 +43,8 @@ public class MessageHandler {
       */
     void handleMessage(Message message, InetAddress address) {
 
-        if(message.getMessageType()!= Message.MessageType.REMOVED && message.getSenderId().equals(peer.getServerId()))
+        if(message.getMessageType()!= Message.MessageType.REMOVED && message.getMessageType()!= Message.MessageType.STORED &&
+                message.getSenderId().equals(peer.getServerId()))
             return;
 
         int randomWait;
@@ -98,9 +99,6 @@ public class MessageHandler {
         ConcurrentHashMap<String, FileInfo> backedUpFiles = controller.getBackedUpFiles();
         for (Map.Entry<String, FileInfo> entry : backedUpFiles.entrySet()) {
             FileInfo fileInfo = entry.getValue();
-            System.out.println("------Fileid1: " + fileId);
-            System.out.println("------Fileid2: " + fileInfo.getFileId());
-            System.out.println("------Result: " + fileInfo.getFileId().equals(fileId));
             if(fileInfo.getFileId().equals(fileId)){
                 return;
             }
