b4e0d277af1a240946a9514ffce7bf2829cdb234
filipeFLemos <ee121402fe.up.pt>
2019-04-04 21:11:35 +0100

    sdadssada

---
+++ src/channels/SocketReceiver.java
@@ -0,0 +1,92 @@
+package channels;
+
+import message.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SocketReceiver implements Runnable {
+
+    private static final int MAX_TCP_SOCKETS = 50;
+
+    private ServerSocket serverSocket;
+
+    private ExecutorService threadPool = Executors.newFixedThreadPool(MAX_TCP_SOCKETS);
+
+    private MessageHandler dispatcher;
+
+    /**
+     * Instantiates a socket receiver
+     *
+     * @param port receiver port
+     * @param dispatcher receiver dispatcher
+     */
+    public SocketReceiver(int port, MessageHandler dispatcher) {
+        this.dispatcher = dispatcher;
+        try {
+            this.serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.out.println("ServerSocket already working, no need to open again");
+        }
+    }
+
+    /**
+     * Method ran when thread starts executing. Submits handler to the thread pool
+     */
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                threadPool.submit(() -> {
+                    try {
+                        socketHandler(socket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Socket handler.
+     *
+     * @param socket the socket
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    private void socketHandler(Socket socket) throws IOException, ClassNotFoundException {
+        ObjectInputStream stream = null;
+
+        try {
+            stream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            System.out.println("Error creating Object Input Stream");
+            e.printStackTrace();
+        }
+
+        Message message;
+        while((message = (Message) stream.readObject()) != null) {
+            dispatcher.handleMessage(message, null);
+            System.out.println("Received CHUNK message " + message.getChunkNo() + " via TCP");
+
+            try {
+                stream = new ObjectInputStream(socket.getInputStream());
+            }
+            catch (IOException e) {
+                System.out.println("Closing TCP socket...");
+                socket.close();
+                break;
+            }
+        }
+    }
+}
