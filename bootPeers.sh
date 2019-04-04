e79a465f25fb22d74a08378263bfd003b2c2956e
Alexandre <alex.al.mar@hotmail.com>
2019-04-04 16:09:32 +0100

    Limits

--- bootPeers.sh
+++ bootPeers.sh
@@ -1,6 +1,7 @@
 gnome-terminal -e "sh reset.sh
                    sh rmi.sh"
 
+sleep 1
 gnome-terminal -e "sh run.sh 1.0 1"
 gnome-terminal -e "sh run.sh 1.0 2"
 gnome-terminal -e "sh run.sh 1.0 3"
