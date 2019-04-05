97736ee6032cb3787838c907125322df3a8c9fff
filipeFLemos <ee121402fe.up.pt>
2019-04-05 23:46:57 +0100

    refactored and reformated

--- src/protocols/BackupInitiator.java
+++ src/protocols/BackupInitiator.java
@@ -11,11 +11,11 @@ import java.io.*;
 import java.util.ArrayList;
 
 
-public class BackupInitiator implements Runnable{
+public class BackupInitiator implements Runnable {
 
     private String filePath;
     private int replicationDegree;
-    private int numberChunks;
+    private int numberOfChunks;
     private ArrayList<Message> chunks;
     private String fileId;
     private File file;
@@ -31,47 +31,48 @@ public class BackupInitiator implements Runnable{
         file = new File(filePath);
         fileId = Utils.getFileID(filePath);
 
-        numberChunks = (int) (file.length() / Globals.MAX_CHUNK_SIZE + 1);
+        numberOfChunks = (int) (file.length() / Globals.MAX_CHUNK_SIZE + 1);
         chunks = new ArrayList<>();
     }
 
     /**
-      * Method executed when thread starts running. Executes the backup protocols as an initiator peer.
-      */
+     * Executes the backup protocol for a file.
+     */
     @Override
     public void run() {
 
         UI.printInfo("-------------- Executing Backup Protocol -------------");
 
-
         splitIntoChunks();
-
-        int tries = 0;
-        int waitTime = 500;
 
         peerState.initPeersWithFile(fileId);
 
-        for(Message chunk : chunks)
+        for (Message chunk : chunks) {
             peerState.listenForSTORED(chunk);
+        }
+
+        int tries = 1;
+        int waitTime = 500;
 
         do {
-            tries++; waitTime *= 2;
             UI.print("Sending " + filePath + " PUTCHUNK messages " + tries + " times");
 
-            if(tries > Globals.MAX_PUTCHUNK_TRIES) {
+            if (tries > Globals.MAX_PUTCHUNK_TRIES) {
                 UI.printError("Aborting backup, attempt limit reached");
                 UI.printInfo("------------------------------------------------------");
                 return;
             }
 
-            for(Message chunk : chunks){
+            for (Message chunk : chunks) {
                 channel.sendMessage(chunk);
                 UI.print("Sending " + chunk.getMessageType() + " message: " + chunk.getChunkNo());
             }
+            tries++;
+            waitTime *= 2;
 
-        } while(!wereAllSTOREDReceived(waitTime));
+        } while (!wereAllSTOREDReceived(waitTime));
 
-        peerState.backUpFile(filePath, fileId, numberChunks);
+        peerState.backUpFile(filePath, fileId, numberOfChunks);
         UI.printOK("File " + filePath + " backed up");
         UI.printInfo("------------------------------------------------------");
     }
@@ -85,17 +86,18 @@ public class BackupInitiator implements Runnable{
             FileInputStream fileStream = new FileInputStream(file);
             BufferedInputStream bufferedFile = new BufferedInputStream(fileStream);
 
-            for(int i = 0; i < numberChunks; i++) {
+            for (int i = 0; i < numberOfChunks; i++) {
                 byte[] body;
                 byte[] aux = new byte[Globals.MAX_CHUNK_SIZE];
                 int bytesRead = bufferedFile.read(aux);
 
-                if(bytesRead == -1)
+                if (bytesRead == -1) {
                     body = new byte[0];
-                else if(bytesRead < Globals.MAX_CHUNK_SIZE)
+                } else if (bytesRead < Globals.MAX_CHUNK_SIZE) {
                     body = new byte[bytesRead];
-                else
+                } else {
                     body = new byte[Globals.MAX_CHUNK_SIZE];
+                }
 
                 System.arraycopy(aux, 0, body, 0, body.length);
                 chunks.add(new Message(peerState.getVersion(), peerState.getServerId(), fileId, body, Message.MessageType.PUTCHUNK, i, replicationDegree));
@@ -106,10 +108,11 @@ public class BackupInitiator implements Runnable{
     }
 
     /**
-      * After the given waitTime, checks if all chunks have achieved their desired replication degree, while removing those that have from the chunks container.
-      * @param waitTime - the delay before starting to check
-      * @return true if all the chunks achieved their desired replication degree and false if otherwise
-      */
+     * After the given waitTime, checks if all chunks have achieved their desired replication degree, while removing those that have from the chunks container.
+     *
+     * @param waitTime - the delay before starting to check
+     * @return true if all the chunks achieved their desired replication degree and false if otherwise
+     */
     private boolean wereAllSTOREDReceived(int waitTime) {
         try {
             Thread.sleep(waitTime);
@@ -117,7 +120,7 @@ public class BackupInitiator implements Runnable{
             e.printStackTrace();
         }
 
-        chunks.removeIf( chunk -> peerState.getChunkRepDeg(chunk) >= chunk.getReplicationDeg());
+        chunks.removeIf(chunk -> peerState.getChunkRepDeg(chunk) >= chunk.getReplicationDeg());
 
         return chunks.isEmpty();
     }
