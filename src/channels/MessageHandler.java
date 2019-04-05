e1f08dbb9c57fa489e3585c88ef50d614596e5fc
filipeFLemos <ee121402fe.up.pt>
2019-04-05 20:10:56 +0100

    debug

--- src/channels/MessageHandler.java
+++ src/channels/MessageHandler.java
@@ -49,6 +49,7 @@ public class MessageHandler {
         }
 
         int randomWait;
+        System.out.println("MessageType: " + message.getMessageType());
         switch(message.getMessageType()) {
             case PUTCHUNK:
                 if(!message.getVersion().equals("1.0")) {
